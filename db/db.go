98db60fa488dca8e4a0f5ad8f4b808ec38a6786c
Mahdi Bouaziz <mahdiibouaziz@gmail.com>
2024-12-16 11:48:28 +0100

    feat: add user db

--- db/db.go
+++ db/db.go
@@ -26,6 +26,19 @@ func InitDB() {
 
 // Create tables for the DB if they don't exists
 func createTables() {
+	createUsersTable := `
+		CREATE TABLE IF NOT EXISTS users (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			email TEXT NOT NULL UNIQUE,
+			password TEXT NOT NULL
+		)
+	`
+	_, err := DB.Exec(createUsersTable)
+	if err != nil {
+		log.Fatal(err)
+		panic("could not create users table")
+	}
+
 	createEventsTable := `
 		CREATE TABLE IF NOT EXISTS events (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -33,10 +46,11 @@ func createTables() {
 			description TEXT NOT NULL,
 			location TEXT NOT NULL,
 			dateTime DATETIME NOT NULL,
-			user_id INTEGER
+			user_id INTEGER,
+			FOREIGN KEY (user_id) REFERENCES users(id)
 		)
 	`
-	_, err := DB.Exec(createEventsTable)
+	_, err = DB.Exec(createEventsTable)
 	if err != nil {
 		log.Fatal(err)
 		panic("could not create events table")
